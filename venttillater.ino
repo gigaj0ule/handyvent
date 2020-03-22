97606d663147ed852adbb68774a53b836d15cde1
jake waters <jerkey@g-cipher.net>
2020-03-22 00:29:20 -0700

    the analogWrite is commented out so this should change nothing

--- venttillater.ino
+++ venttillater.ino
@@ -1,13 +1,17 @@
 #define VERSIONSTRING   "VENTTILLATER.git version 1.0" // 16 characters per line
 #define SERIAL_BAUD_RATE        57600
 
-#define TIME_FILL_MIN   100ul
-#define TIME_FILL_MAX   1000ul
-#define TIME_INHALE_MIN   500ul
-#define TIME_INHALE_MAX   1000ul
-#define TIME_EXHALE_MIN   100ul
-#define TIME_EXHALE_MAX   3000ul
-#define TIME_NEITHER_OPEN       100ul // time between closing one valve and opening another
+#define TIME_FILL_MIN      100ul
+#define TIME_FILL_MAX     5000ul
+#define TIME_INHALE_MIN   2000ul
+#define TIME_INHALE_MAX   5000ul
+#define TIME_EXHALE_MIN   2000ul
+#define TIME_EXHALE_MAX   5000ul
+#define TIME_NEITHER_OPEN  100ul // time between closing one valve and opening another
+#define TIME_VALVE_FULLCURRENT 50 // time to give solenoid full current
+
+#define PWM_VALVE_GRAB  255 // assuming power source matches solenoid voltage this is 255
+#define PWM_VALVE_HOLD  100 // PWM value to keep solenoid latched
 
 #define KNOB_FILL       A0
 #define KNOB_INHALE     A1
@@ -41,14 +45,14 @@ uint32_t time_start_ventilate; // record the time at start of cycle
 
 void ventilate() {
   uint32_t cycle_time = millis() - time_start_ventilate;
-  digitalWrite(VALVE_FILL, (cycle_time < time_fill)
-                        && (cycle_time < time_exhale)); // set fill valve
+  setSolenoidState(VALVE_FILL, (cycle_time < time_fill)
+                            && (cycle_time < time_exhale)); // set fill valve
   // FILL VALVE WILL CLOSE WHEN EXHALE ENDS REGARDLESS OF FILL TIME SETTING
 
-  digitalWrite(VALVE_EXHALE, cycle_time < time_exhale); // set exhale valve
+  setSolenoidState(VALVE_EXHALE, cycle_time < time_exhale); // set exhale valve
 
-  digitalWrite(VALVE_INHALE, (cycle_time > time_exhale + TIME_NEITHER_OPEN)
-                          && (cycle_time < (time_inhale + time_exhale + TIME_NEITHER_OPEN))); // set inhale valve
+  setSolenoidState(VALVE_INHALE, (cycle_time > time_exhale + TIME_NEITHER_OPEN)
+                              && (cycle_time < (time_inhale + time_exhale + TIME_NEITHER_OPEN))); // set inhale valve
 
   if (cycle_time > (time_inhale + time_exhale + (TIME_NEITHER_OPEN * 2))) {
     time_start_ventilate = millis(); // record the present time and restart the cycle
@@ -88,11 +92,29 @@ void handleSerial() {
   }
 }
 
+void setSolenoidState(uint16_t pinNumber, bool setState) {
+  static uint8_t pinPwmValue[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // store the state of the pin
+  if (setState && (pinPwmValue[pinNumber] == 0)) { // if pin was off
+    Serial.print("pin "+String(pinNumber)+" OPEN	");
+    digitalWrite(pinNumber, HIGH); // analogWrite(pinNumber, PWM_VALVE_GRAB);
+    delay(TIME_VALVE_FULLCURRENT);
+    //analogWrite(pinNumber, PWM_VALVE_HOLD);
+    pinPwmValue[pinNumber] = PWM_VALVE_HOLD; // store pwm value
+  } else if ((! setState) && (pinPwmValue[pinNumber] > 0)) {
+    Serial.print("pin "+String(pinNumber)+" CLOSE	");
+    digitalWrite(pinNumber, LOW);
+    pinPwmValue[pinNumber] = 0; // store pwm value
+  }
+}
+
 void setup() {
   pinMode(VALVE_FILL  ,OUTPUT);
   pinMode(VALVE_INHALE,OUTPUT);
   pinMode(VALVE_EXHALE,OUTPUT);
   pinMode(BEEPER_PIN  ,OUTPUT);
+
+  setPwmFrequency(5, 8); // set PWM frequency for pins 5 and 6 to 31250Hz
+  setPwmFrequency(9, 8); // set PWM frequency for pins 9 and 10 to 31250Hz
 
   Serial.begin(SERIAL_BAUD_RATE); // for diag
   Serial.println(VERSIONSTRING);
@@ -110,4 +132,63 @@ void loop() {
   checkKnobs();
   ventilate();
   delay(50); // TODO should not need this
+}
+
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+    case 1:
+      mode = 0x01;
+      break;
+    case 8:
+      mode = 0x02;
+      break;
+    case 64:
+      mode = 0x03;
+      break;
+    case 256:
+      mode = 0x04;
+      break;
+    case 1024:
+      mode = 0x05;
+      break;
+    default:
+      return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    }
+    else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  }
+  else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+    case 1:
+      mode = 0x01;
+      break;
+    case 8:
+      mode = 0x02;
+      break;
+    case 32:
+      mode = 0x03;
+      break;
+    case 64:
+      mode = 0x04;
+      break;
+    case 128:
+      mode = 0x05;
+      break;
+    case 256:
+      mode = 0x06;
+      break;
+    case 1024:
+      mode = 0x7;
+      break;
+    default:
+      return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
 }
