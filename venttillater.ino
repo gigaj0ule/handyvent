f5d7c8efd0660dc6e7a5e850e52eeebe5d9e71ef
adammunich <me@adammunich.com>
2020-03-30 00:51:21 -0700

    update.ino

--- venttillater.ino
+++ venttillater.ino
@@ -1,43 +1,49 @@
 #define VERSIONSTRING   "VENTTILLATER.git version 1.0" // 16 characters per line
-#define SERIAL_BAUD_RATE        57600
+#define SERIAL_BAUD_RATE            57600
 
-#define CYCLES_PER_MINUTE_MIN       6
-#define CYCLES_PER_MINUTE_MAX       25
+#define CYCLES_PER_MINUTE_MIN       10
+#define CYCLES_PER_MINUTE_MAX       40
 #define INHALE_EXHALE_RATIO_MIN     1.0f // technically actually exhale/inhale ratio
 #define INHALE_EXHALE_RATIO_MAX     5.0f // but this seems common terminology
 
-#define TIME_FILL_MIN      100ul
-#define TIME_FILL_MAX     5000ul
+#define TIME_FILL_MIN               100ul
+#define TIME_FILL_MAX               5000ul
 
-#define TIME_NEITHER_OPEN  100ul // time between closing one valve and opening another
-#define TIME_VALVE_FULLCURRENT 50 // time to give solenoid full current
+#define TIME_NEITHER_OPEN           100ul // time between closing one valve and opening another
+#define TIME_VALVE_FULLCURRENT      50 // time to give solenoid full current
 
-#define PWM_VALVE_GRAB  255 // assuming power source matches solenoid voltage this is 255
-#define PWM_VALVE_HOLD  100 // PWM value to keep solenoid latched
+#define PWM_VALVE_GRAB              255 // assuming power source matches solenoid voltage this is 255
+#define PWM_VALVE_HOLD              100 // PWM value to keep solenoid latched
 
-#define KNOB_FILL                   A0
+#define KNOB_FILL                   A2
 #define KNOB_CYCLES_PER_MINUTE      A1
-#define KNOB_INHALE_EXHALE_RATIO    A2
-#define OVERSAMPLES     25 // number of times analogRead() is repeated per read
+#define KNOB_INHALE_EXHALE_RATIO    A0
+#define OVERSAMPLES                 25 // number of times analogRead() is repeated per read
 
-#define VALVE_FILL              9
-#define VALVE_INHALE_OPEN       10
-#define VALVE_INHALE_CLOSE      11
-#define VALVE_EXHALE_OPEN       6
-#define VALVE_EXHALE_CLOSE      3
+#define VALVE_FILL              6
+#define VALVE_INHALE_OPEN       9
+#define VALVE_INHALE_CLOSE      10
+#define VALVE_EXHALE_OPEN       10
+#define VALVE_EXHALE_CLOSE      9
 
-#define BEEPER_PIN      A3      // connect a speaker or beeper
-#define BADTONE	        200	// bad tone frequency
-#define GOODTONE	1000	// good tone frequency
+#define BEEPER_PIN              12    // connect a speaker or beeper
+#define BADTONE	                1000	// bad tone frequency
+#define GOODTONE	              2000	// good tone frequency
 
-#define LCD_RS 		12	// LCD R/W pin to ground
-#define LCD_Enable 	13	// LCD VSS pin to ground
-#define LCD_D4 		2	// LCD VCC pin to 5V
-#define LCD_D5 		4	// 10K potentiometer pins: (GND)---(LCD_VO)---(+5v)
-#define LCD_D6 		7	// ends to +5V and ground
-#define LCD_D7 		8	// wiper to LCD VO pin (pin 3)
-#define LCD_COLUMNS     16	// 
-#define LCD_ROWS 	2	// 
+// LCD R/W pin to ground
+// LCD VSS pin to ground
+// LCD VCC pin to 5V
+// 10K potentiometer pins: (GND)---(LCD_VO)---(+5v)
+// ends to +5V and ground
+// wiper to LCD VO pin (pin 3)
+#define LCD_RS 		      2	
+#define LCD_Enable 	    3	
+#define LCD_D4 		      4	
+#define LCD_D5 		      5	
+#define LCD_D6 		      7	
+#define LCD_D7 		      8	
+#define LCD_COLUMNS     16 
+#define LCD_ROWS 	      2
 
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(LCD_RS, LCD_Enable, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
@@ -49,18 +55,25 @@ uint32_t time_start_ventilate; // record the time at start of cycle
 
 void ventilate() {
   uint32_t cycle_time = millis() - time_start_ventilate;
+  
   setFillSolenoidState((cycle_time < time_fill)
                             && (cycle_time < time_exhale)); // set fill valve
   // FILL VALVE WILL CLOSE WHEN EXHALE ENDS REGARDLESS OF FILL TIME SETTING
 
   setInhaleValveState(cycle_time < time_exhale); // set exhale valve
 
+  /*
   setExhaleValveState((cycle_time > time_exhale + TIME_NEITHER_OPEN)
                               && (cycle_time < (time_inhale + time_exhale + TIME_NEITHER_OPEN))); // set inhale valve
+  */
 
   if (cycle_time > (time_inhale + time_exhale + (TIME_NEITHER_OPEN * 2))) {
-    time_start_ventilate = millis(); // record the present time and restart the cycle
-    Serial.print("Restart time_start_ventilate at "+String(time_start_ventilate));
+
+    // record the present time and restart the cycle
+    time_start_ventilate = millis(); 
+    
+    Serial.println("Restart time_start_ventilate at "+String(time_start_ventilate));
+    
     Serial.println("	time_fill:"+String(time_fill)+"	time_inhale:"+String(time_inhale)+"	time_exhale:"+String(time_exhale));
   }
 }
@@ -68,13 +81,13 @@ void ventilate() {
 void setFillSolenoidState(bool setState) {
   static uint8_t pwmValue = 0; // store the state of the pin
   if (setState && (pwmValue == 0)) { // if pin was off
-    Serial.print("fill OPEN	");
+    Serial.println("fill OPEN	");
     digitalWrite(VALVE_FILL, HIGH); // analogWrite(VALVE_FILL, PWM_VALVE_GRAB);
     //delay(TIME_VALVE_FULLCURRENT);
     //analogWrite(VALVE_FILL, PWM_VALVE_HOLD);
     pwmValue = PWM_VALVE_HOLD; // store pwm value
   } else if ((! setState) && (pwmValue > 0)) {
-    Serial.print("fill CLOSE	");
+    Serial.println("fill CLOSE	");
     digitalWrite(VALVE_FILL, LOW);
     pwmValue = 0; // store pwm value
   }
@@ -83,12 +96,12 @@ void setFillSolenoidState(bool setState) {
 void setInhaleValveState(bool setState) {
   static bool valveState = false; // store the state of the valve
   if (setState && (valveState == false)) { // if the valve was closed and we want it open
-    Serial.print("inhale  OPEN	");
+    Serial.println("inhale  OPEN	");
     digitalWrite(VALVE_INHALE_OPEN, HIGH);
     digitalWrite(VALVE_INHALE_CLOSE, LOW);
     valveState = true; // store valve state
   } else if ((! setState) && (valveState == true)) {
-    Serial.print("inhale  CLOSE	");
+    Serial.println("inhale  CLOSE	");
     digitalWrite(VALVE_INHALE_CLOSE, HIGH);
     digitalWrite(VALVE_INHALE_OPEN, LOW);
     valveState = false; // store valve state
@@ -98,12 +111,12 @@ void setInhaleValveState(bool setState) {
 void setExhaleValveState(bool setState) {
   static bool valveState = false; // store the state of the valve
   if (setState && (valveState == false)) { // if the valve was closed and we want it open
-    Serial.print("exhale  OPEN	");
+    Serial.println("exhale  OPEN	");
     digitalWrite(VALVE_EXHALE_OPEN, HIGH);
     digitalWrite(VALVE_EXHALE_CLOSE, LOW);
     valveState = true; // store valve state
   } else if ((! setState) && (valveState == true)) {
-    Serial.print("exhale  CLOSE	");
+    Serial.println("exhale  CLOSE	");
     digitalWrite(VALVE_EXHALE_CLOSE, HIGH);
     digitalWrite(VALVE_EXHALE_OPEN, LOW);
     valveState = false; // store valve state
